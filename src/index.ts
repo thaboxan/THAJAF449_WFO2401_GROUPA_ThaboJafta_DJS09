f208c523a89c13f43ed53a1c59ca258d6189bdee
Thabo <thvbojafta@gmail.com>
2024-06-10 17:20:10 +0200

    create enum

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,36 @@
-// Add the properties to the Dashboard
-// This is a continuation of the Challenge, in which you are asked to add
-// the 3 properties image and title to the dashboard based on the 
-// properties array
 
 const propertyContainer = document.querySelector('.properties')
 const footer = document.querySelector('.footer')
 
 import { showReviewTotal, populateUser } from '../utils'
-//@ts-ignore
+import { Permissions, LoyaltyUser } from './enums'
+
 let isOpen: boolean
+
 
 // Reviews
 const reviews : { 
     name: string; 
     stars: number; 
-    loyaltyUser: boolean; 
+    loyaltyUser: LoyaltyUser; 
     date: string
     }[] = [
     {
         name: 'Sheia',
         stars: 5,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
         date: '01-04-2021'
     },
     {
         name: 'Andrzej',
         stars: 3,
-        loyaltyUser: false,
+        loyaltyUser: LoyaltyUser.BRONZE_USER,
         date: '28-03-2021'
     },
     {
         name: 'Omar',
         stars: 4,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.SILVER_USER,
         date: '27-03-2021'
     },
 ]
@@ -41,12 +39,15 @@ const reviews : {
 const you: {
     firstName: string;
     lastName: string;
+    permissions: Permissions.ADMIN;
+    loyaltyUser: LoyaltyUser; 
     isReturning: boolean;
     age: number;
     stayedAt: string[]
 } = {
     firstName: 'Bobby',
     lastName: 'Brown',
+    permissions: Permissions.ADMIN,
     isReturning: true,
     age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
@@ -127,6 +128,7 @@ for (let i = 0; i < properties.length; i++) {
 
 
 let currentLocation: [string, string, number] = ['White River', '15:10', 23]
-footer.innerHTML = `${currentLocation[0]}  ${currentLocation[1]}  ${currentLocation[2]} °`
+//@ts-ignore
+footer.innerHTML = `${currentLocation[0]}  ${currentLocation[1]}  ${currentLocation[2]}°`
 
 
